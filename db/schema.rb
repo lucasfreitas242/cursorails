45bdeac9d13f9fff566ae0212a8d54fd6faaf9f3
Lucas Freitas <lucasfrittas@gmail.com>
2021-03-20 02:17:02 -0300

    Adicionando a Model Contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_20_045650) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
